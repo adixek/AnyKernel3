9c908120de0a0cb392821373c5269e8b1c552a9c
adixek <kaz14adi@gmail.com>
2026-03-22 17:27:02 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -29,7 +29,7 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/by-name/boot;
+#BLOCK=/dev/block/by-name/boot;
 IS_SLOT_DEVICE=1;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
